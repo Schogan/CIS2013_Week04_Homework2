88535a407f270acd08251a918ee1702b1e0b34b4
Schogan <navy15@gmail.com>
2018-02-13 15:44:49 -0600

    Complete Game with A selection
    once A has been selected it is no longer able to change a second time.

--- CIS2013_Week04_Homework2.cpp
+++ CIS2013_Week04_Homework2.cpp
@@ -16,17 +16,70 @@ int player_total = 0;
 int dealer_total = 0;
 
 
-
 int Player_hand(int Player_Start){
 		
 	if(Player_Start == 1){ 
 		char stay_hit = 'a';
 		char yes_no = 'a';
-		int player_first_card = rand() % 10 + 1;
-		int player_second_card = rand() % 10 + 1;
-		
-			cout << "your first cards are " << player_first_card <<" and " 
-				<< player_second_card << endl;
+				
+			cout << "your first cards are ";
+			
+				int player_first_card = rand() % 13 + 1;
+				
+				if (player_first_card == 10){
+					cout << "J";
+					player_first_card = 10;
+				}
+				if (player_first_card == 11){
+					cout << "Q";
+					player_first_card = 10;
+				}
+				if (player_first_card == 12){
+					cout << "K";
+					player_first_card = 10;
+				}
+				if ((player_first_card == 13) || (player_first_card == 1)){
+					cout << "A";
+					player_first_card = 11;
+				}
+				if ((player_first_card <= 9) && (player_first_card >= 2)){
+					cout << player_first_card;
+				}
+				
+				cout <<" and ";
+				
+				int player_second_card = rand() % 13 + 1;
+				
+				if (player_second_card == 10){
+					cout << "J";
+					player_second_card = 10;
+				}
+				if (player_second_card == 11){
+					cout << "Q";
+					player_second_card = 10;
+				}
+				if (player_second_card == 12){
+					cout << "K";
+					player_second_card = 10;
+				}
+				if ((player_second_card == 13) || (player_second_card == 1)){
+					cout << "A";
+					player_second_card = 11;
+				}
+				if ((player_second_card <= 9) && (player_second_card >= 2)){
+					cout << player_second_card;
+				}
+				
+			player_total = player_first_card + player_second_card;
+				
+				if ((player_total > 21) && (player_first_card == 11)){
+					player_first_card = 1;
+					
+				}
+				if ((player_total > 21) && (player_second_card == 11)){
+					player_second_card = 1;
+					
+				}
 			player_total = player_first_card + player_second_card;
 			cout << " for a total of: " << player_total << endl;
 			
@@ -35,12 +88,47 @@ int Player_hand(int Player_Start){
 					cout << "Do you want to stay or hit? (s or h)" << endl;
 					cin >> stay_hit;
 					
-					int player_hit = rand() % 10 + 1;
+					
 					
 					if (stay_hit == 'h'){
-						cout << "You chose to hit your new card is a " << player_hit << endl;
+						cout << "You chose to hit your new card is a "; 
+						
+						int player_hit = rand() % 13 + 1;
+						
+						if (player_hit == 10){
+							cout << "J";
+							player_hit = 10;
+						}
+						if (player_hit == 11){
+							cout << "Q";
+							player_hit = 10;
+						}
+						if (player_hit == 12){
+							cout << "K";
+							player_hit = 10;
+						}
+						if ((player_hit == 13) || (player_hit == 1)){
+							cout << "A";
+							int player_hit_test = 11;
+							
+							int player_total_test = player_total + player_hit_test;
+							if (player_total_test > 21){
+								player_hit = 1;
+							}
+							if (player_total_test <= 21){
+								player_hit = 11;
+							}
+						}
+											
+						if ((player_hit <= 9) && (player_hit >= 2)){
+							cout << player_hit;
+						}
+						cout << endl;
+						
 						player_total = player_total + player_hit;
-						cout << " for a new total of " << player_total << endl;
+						if (player_total <= 21){
+							cout << " for a new total of " << player_total << endl;
+						}
 						if (player_total > 21){
 							cout << "You bust!!!" << endl;
 							player_bust = true;
@@ -74,25 +162,110 @@ int Player_hand(int Player_Start){
 			}
 	}		
 }
+
+
 int Dealer_hand(int Dealer_Start){
 	
-	int dealer_first_card = rand() % 10 + 1;
-	int dealer_second_card = rand() % 10 + 1;
-	char yes_no = 'a';
+	
 	
 	if (Dealer_Start == 1){
-		cout << "The dealers first card is " << dealer_first_card << endl;
-	
-		while ((dealer_stay == false) && (dealer_bust == false)){
+		char yes_no = 'a';
+		
+		cout << "Dealer's first cards are ";
+		while ((dealer_stay == false) && (dealer_bust == false)){	
+				int dealer_first_card = rand() % 13 + 1;
+				
+				if (dealer_first_card == 10){
+					cout << "J";
+					dealer_first_card = 10;
+				}
+				if (dealer_first_card == 11){
+					cout << "Q";
+					dealer_first_card = 10;
+				}
+				if (dealer_first_card == 12){
+					cout << "K";
+					dealer_first_card = 10;
+				}
+				if ((dealer_first_card == 13) || (dealer_first_card == 1)){
+					cout << "A";
+					dealer_first_card = 11;
+				}
+				if ((dealer_first_card <= 9) && (dealer_first_card >= 2)){
+					cout << dealer_first_card;
+				}
+				
+				cout <<" and ";
+				
+				int dealer_second_card = rand() % 13 + 1;
+				
+				if (dealer_second_card == 10){
+					cout << "J";
+					dealer_second_card = 10;
+				}
+				if (dealer_second_card == 11){
+					cout << "Q";
+					dealer_second_card = 10;
+				}
+				if (dealer_second_card == 12){
+					cout << "K";
+					dealer_second_card = 10;
+				}
+				if ((dealer_second_card == 13) || (dealer_second_card == 1)){
+					cout << "A";
+					dealer_second_card = 11;
+				}
+				if ((dealer_second_card <= 9) && (dealer_second_card >= 2)){
+					cout << dealer_second_card;
+				}
+				
+			dealer_total = dealer_first_card + dealer_second_card;
+				
+				if ((dealer_total > 21) && (dealer_first_card == 11)){
+					dealer_first_card = 1;
 					
-				cout << "Dealer's next card is " << dealer_second_card << endl;
-				dealer_total = dealer_first_card + dealer_second_card;
-				cout << "For a total of " << dealer_total << endl;
+				}
+				if ((dealer_total > 21) && (dealer_second_card == 11)){
+					dealer_second_card = 1;
+					
+				}
+			dealer_total = dealer_first_card + dealer_second_card;
+			cout << " for a total of: " << dealer_total << endl;
+			
+		
 				while (dealer_total <= 16){
+						
+					cout << "Dealer hits dealer's next card is a ";
+						
+					int dealer_hit = rand() % 13 + 1;
+						if (dealer_hit == 10){
+							cout << "J";
+							dealer_hit = 10;
+						}
+						if (dealer_hit == 11){
+							cout << "Q";
+							dealer_hit = 10;
+						}
+						if (dealer_hit == 12){
+							cout << "K";
+							dealer_hit = 10;
+						}
+						if ((dealer_hit == 13) || (dealer_hit == 1)){
+							cout << "A";
+							int dealer_hit_test = 11;
 							
-					int dealer_hit = rand() % 10 + 1;
-							
-					cout << "Dealer hits dealer's next card is a " << dealer_hit << endl;
+							int dealer_total_test = dealer_total + dealer_hit_test;
+							if (dealer_total_test > 21){
+								dealer_hit = 1;
+							}
+							if (dealer_total_test <= 21){
+								dealer_hit = 11;
+							}
+						}
+						if ((dealer_hit <= 9) && (dealer_hit >= 2)){
+							cout << dealer_hit;
+						}
+					 cout << endl;
 					dealer_total = dealer_total + dealer_hit;
 					cout << " for a new total of " << dealer_total << endl;
 				}
